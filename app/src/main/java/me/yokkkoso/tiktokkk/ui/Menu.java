75f356c722a3186f390079d9203d79781e7d38d0
yokkkoso <yokko.jke@gmail.com>
2026-07-13 00:10:13 +1000

    chore: release v1.1.2

--- app/src/main/java/me/yokkkoso/tiktokkk/ui/Menu.java
+++ app/src/main/java/me/yokkkoso/tiktokkk/ui/Menu.java
@@ -28,7 +28,7 @@ import me.yokkkoso.tiktokkk.Prefs;
 public final class Menu {
 
     public static final boolean DEV = false;
-    static final String VERSION = "1.1.1";
+    static final String VERSION = "1.1.2";
 
     private static Dialog CURRENT;
     private static FrameLayout HOST;
